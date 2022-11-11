33856ed6c2b7178fda46820adac464a4cf8f6e9d
dom <domagoj.kriskovic@gmail.com>
2022-11-11 16:11:14 +0100

    feat: pkg installer for mac

--- build-darwin.sh
+++ build-darwin.sh
@@ -12,12 +12,22 @@ cp ./config.ini ./dist/CircuitBlocksAgent.app/Contents/MacOS/config.ini
 # for this to work, credentials need to be stored in keychain
 
 #  xcrun notarytool store-credentials circuitblocks
-#                --apple-id "albert@circuitmess.com"
+#                --apple-id "email@email.com"
 #                --team-id KLW4LV446H
 #                --password <app_id_password>
 
 cd ./dist
 codesign --deep --force --options=runtime --sign "1CF7C6C4A31BA61D9F4C5E3D97C53157CA13C26C" --timestamp ./CircuitBlocksAgent.app
-create-dmg --overwrite "CircuitBlocksAgent.app"
-mv ./CircuitBlocksAgent\ 1.0.64.dmg ./CircuitblocksAagent-osx.dmg
-xcrun notarytool submit ./CircuitblocksAagent-osx.dmg --keychain-profile circuitblocks --wait
+# create-dmg --overwrite "CircuitBlocksAgent.app"
+# mv ./CircuitBlocksAgent\ 1.0.64.dmg ./CircuitBlocksAgent-osx.dmg
+# xcrun notarytool submit ./CircuitBlocksAgent-osx.dmg --keychain-profile circuitblocks --wait
+mkdir ./payload
+mkdir ./payload/Applications
+mkdir ./payload/Library
+mkdir ./payload/Library/LaunchAgents
+mv ./CircuitBlocksAgent.app ./payload/Applications
+cp ../pkg-assets/CircuitBlocksAgent.plist ./payload/Library/LaunchAgents
+cp ../pkg-assets/pkg-installer.plist ./pkg-installer.plist
+pkgbuild --root ./payload --ownership preserve --identifier com.circuitmess.pkg.CircuitBlocksAgent --component-plist pkg-installer.plist --sign "010471773D206582CE44C296760B8E01FA57C8EC" ./circuitblocks-agent.pkg
+productsign --sign "87B75286D0D0D8C205B28BC29ADEFAB0AE9DD903" ./circuitblocks-agent.pkg ./circuitblocks-agent-signed.pkg
+xcrun notarytool submit ./circuitblocks-agent-signed.pkg --keychain-profile circuitblocks --wait
